8e84478daf2fd257833afe15a8ba3768b0e25f5a
Oscar Perez-Hernandez <perezoscar360@gmail.com>
2023-07-27 14:05:47 -0500

    adding rollbar

--- server/server.js
+++ server/server.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '1f6399041b17434f9e169b7a60902be7',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+app.use(express.json());
 app.use(express.static(`public`))
 
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,'../public/index.html'))
+})
+
+app.post('/api/guitar', (req, res) => {
+    let {name} = req.body;
+
+    try{
+        nonExistentFunction()
+    } catch(err) {
+        console.error(err);
+    }
 })
 
 app.listen(4000, () => console.log(`server running on 4000`))
